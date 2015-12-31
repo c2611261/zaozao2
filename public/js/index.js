8a40c6e71d1f08ae4413d55c6648ae26c7de1aff
ZhaoYi <yzzhao@thoughtworks.com>
2015-12-31 11:41:35 +0800

    [zhaoyi] integreate index.html

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,14 @@
-angular.module('indexModule', []).
+angular.module('indexModule', ['ngRoute']).
+config(['$locationProvider', function($locationProvider){
+	$locationProvider.html5Mode(true);
+}]).
 controller('IndexController', ['$scope', '$http', '$location',
 	function($scope, $http, $location) {
 		var util = new DomainNameUtil($location);
 		initTagBackground();
-		//console.log('params=', $location.search().code);
+		console.log('params=', $location.search().code);
 		if($location.search().code !== undefined){
-			
+
 		}
 		$http.get(util.getBackendServiceUrl() + "/course_tags")
 			.success(function(e) {
@@ -20,13 +23,12 @@ controller('IndexController', ['$scope', '$http', '$location',
 
 		function initTagBackground() {
 			$scope.courseTags = new Array();
-			$scope.courseTags[0] = {};
+			for(var i=0; i<4; i++){
+				$scope.courseTags = {};
+			}
 			$scope.courseTags[0].backgroundCls = 'yellow';
-			$scope.courseTags[1] = {};
 			$scope.courseTags[1].backgroundCls = 'light-brown';
-			$scope.courseTags[2] = {};
 			$scope.courseTags[2].backgroundCls = 'green';
-			$scope.courseTags[3] = {};
 			$scope.courseTags[3].backgroundCls = 'dark-brown';
 		}
 	}
