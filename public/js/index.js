c767a4f9a41cce73b492c98feb1ef2b1dadbd309
ZhaoYi <yzzhao@thoughtworks.com>
2015-12-31 17:56:16 +0800

    [zhaoyi] integrate new courses on index.html

--- public/js/index.js
+++ public/js/index.js
@@ -13,18 +13,28 @@ controller('IndexController', ['$scope', '$http', '$location',
 		$http.get(util.getBackendServiceUrl() + "/course_tags")
 			.success(function(e) {
 				console.log('get course tags:', e);
-				
 				for (var i = 0; i < e.length; i++) {
 					$scope.courseTags[i].imageUrl =
 						util.getResourceUrl(e[i].imageUrl);
 				}
 				console.log('resource url:', $scope.courseTags);
 			});
+		$http.get(util.getBackendServiceUrl() + "/course/proposal/query")
+		.success(function(e){	
+			console.log('get course ',e);
+			$scope.courses = e;
+			for(var i =0; i<$scope.courses.length;i++){
+				$scope.courses[i].imageUrl = util.getResourceUrl(e[i].titleImageUrl);
+				$scope.courses[i].tags = $scope.courses[i].tags.split(',');
+				
+			}
+		}).error(function(e){
 
+		});
 		function initTagBackground() {
 			$scope.courseTags = new Array();
 			for(var i=0; i<4; i++){
-				$scope.courseTags = {};
+				$scope.courseTags[i] = {};
 			}
 			$scope.courseTags[0].backgroundCls = 'yellow';
 			$scope.courseTags[1].backgroundCls = 'light-brown';
@@ -32,4 +42,11 @@ controller('IndexController', ['$scope', '$http', '$location',
 			$scope.courseTags[3].backgroundCls = 'dark-brown';
 		}
 	}
-]);
+]).directive('backImage',function(){
+	return function(scope, element, attrs){
+        var url = attrs.backImage;
+        element.css({
+            'background-image': 'url(' + url +')'
+        });
+    };
+});
